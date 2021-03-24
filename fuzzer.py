81a35f432d3ec5b45b90db1b598aa51118ba6e11
Drakula <70257617+CYBERDRAKULA@users.noreply.github.com>
2021-03-24 06:49:32 -0400

    Add files via upload

---
+++ fuzzer.py
@@ -0,0 +1,22 @@
+import pyfiglet
+word = pyfiglet.figlet_format('FUZZER')
+b = ("TOOL BY DRAKULA \n")
+c = ("ENTER THE WEBSITE WHICH DO YOU WANT TO SCAN")
+print(word + b + c)  
+from socket import *
+import time
+startTime = time.time()
+
+if __name__ == '__main__':
+   target = input('Enter the host to be scanned: ')
+   t_IP = gethostbyname(target)
+   print ('Starting scan on host: ', t_IP)
+   
+   for i in range(50, 500):
+      s = socket(AF_INET, SOCK_STREAM)
+      
+      conn = s.connect_ex((t_IP, i))
+      if(conn == 0) :
+         print ('Port %d: OPEN' % (i,))
+      s.close()
+print('Time taken:', time.time() - startTime)
